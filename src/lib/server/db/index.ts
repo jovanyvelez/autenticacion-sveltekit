5cfd30d12ae2f8f1f30c4969c3cd669bc934afa4
jovany <jvelez23@misena.edu.co>
2024-11-16 20:31:10 -0500

    inicio11

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -4,7 +4,6 @@ import { dev } from '$app/environment';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 
-console.log('DATABASE_URL', DATABASE_URL);
 
 if (!DATABASE_URL) throw new Error('Problemas en el archivo de conexión a la base de datos lib/server/db/index.ts, primer if');
 
